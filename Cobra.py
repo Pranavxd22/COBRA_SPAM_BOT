0baaa0e5bc96d748e0260f9d296e2a95e1dd9391
Pranavxd22 <101984134+Pranavxd22@users.noreply.github.com>
2022-03-27 08:27:32 +0530

    Update Cobra.py

--- Cobra.py
+++ Cobra.py
@@ -500,8 +500,8 @@ async def spam(e):
                 async with e.client.action(e.chat_id, "typing"):
                     await e.client.send_message(e.chat_id, message)
                     await asyncio.sleep(sleeptime)
-        else:
-            await e.reply(usage, parse_mode=None, link_preview=None)
+                else:
+                    await e.reply(usage, parse_mode=None, link_preview=None)
 
         
 
